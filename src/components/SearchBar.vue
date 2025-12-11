0312c72cebe9713a5bed33cb5b8eb156ce6a6f75
Yeneviel Roberts <yeneviel.roberts.7e8@itb.cat>
2025-12-11 17:44:47 +0100

    estilos

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -11,9 +11,43 @@ const buscarXCategoria=()=>{
 
 <template>
 
-<input type="text" v-model="categoria"/>
-<button @click="buscarXCategoria">Buscar</button>
+<div id="search-bar">
+   <input type="text" v-model="categoria"/>
+    <button @click="buscarXCategoria">Buscar</button> 
+</div>
+
 
 </template>
 
-<style scoped></style>
+<style scoped>
+    input{
+        border-radius: 5px;
+        padding: 5px;
+        width: 300px;
+        border: solid 2px;
+    }
+
+    input:focus{
+        outline-color: blue;
+    }
+
+    button{
+        background-color: #FFE52A;
+        border-radius: 10px;
+        padding: 5px;
+        width: 100px;
+        font-weight: bold;
+        border: 0px;
+        margin: 10px;
+        
+
+    }
+
+    button:hover{
+         box-shadow: 0px 5px 16px  #FFE52A;
+    }
+
+    #search-bar{
+        margin: 10px;
+    }
+</style>
